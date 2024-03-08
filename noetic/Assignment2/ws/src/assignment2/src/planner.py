2286b7b09c72e5c98333f6d00e34cb0a38a8bdf9
Adityaya Dhande <adityaya123@gmail.com>
2024-03-08 17:00:59 +0530

    Static working

--- noetic/Assignment2/ws/src/assignment2/src/planner.py
+++ noetic/Assignment2/ws/src/assignment2/src/planner.py
@@ -9,8 +9,9 @@ import numpy as np
 from numpy import pi as PI
 import math
 
-EPSILON_theta = 0.15
+EPSILON_theta = 0.05
 EPSILON_d = 0.05
+d_star = 0.8
 
 def quat_to_eul(q):
     x, y, z, w = q.x, q.y, q.z, q.w
@@ -59,8 +60,8 @@ class Planner:
         theta = math.atan2(Vf[1], Vf[0])
         delta = (theta - self.psi + PI) % (2*PI) - PI
         d = (self.x_goal - self.x)**2 + (self.y_goal - self.y)**2
-        # print(np.linalg.norm(self.v_attract), np.linalg.norm(self.v_repel))
-        print(delta)
+        # print(delta)
+        print(np.linalg.norm(self.v_attract), np.linalg.norm(self.v_repel))
 
         if delta > EPSILON_theta :
             self.move_cmd.angular.z = 1
@@ -72,8 +73,8 @@ class Planner:
             self.move_cmd.linear.y = 0
         else :
             self.move_cmd.angular.z = 0
-            self.move_cmd.linear.x = min(math.cos(delta), math.cos(delta)) * 3
-            self.move_cmd.linear.y = min(math.sin(delta), math.sin(delta)) * 3
+            self.move_cmd.linear.x = 1 #min(math.cos(delta), math.cos(delta)) 
+            self.move_cmd.linear.y = 0 #min(math.sin(delta), math.sin(delta)) 
 
         if math.sqrt(d) < EPSILON_d :
             self.move_cmd.angular.z = 0
@@ -81,9 +82,8 @@ class Planner:
             self.move_cmd.linear.y = 0
 
     def generate_attraction(self):
-        d = (self.x_goal - self.x)**2 + (self.y_goal - self.y)**2
-        self.v_attract[0] = (self.x_goal - self.x) * 40
-        self.v_attract[1] = (self.y_goal - self.y) * 40
+        self.v_attract[0] = (self.x_goal - self.x) * 100
+        self.v_attract[1] = (self.y_goal - self.y) * 100
 
     def generate_repulsion(self):
         scan_data = self.scan_data
@@ -95,8 +95,9 @@ class Planner:
         count = 0
         x_r, y_r = 0.0, 0.0
         for i in range(N):
-            if scan[i] < 100 and scan[i] > 0.1 :
-                repulsion = 0.2 / scan[i]
+            if scan[i] < d_star : #and scan[i] > 0.1 :
+                repulsion = 1 / scan[i]
+                # repulsion = (1/d_star - 1/scan[i]) * (1 / scan[i]**2)
                 x_r -= repulsion * math.cos(angle_min + psi + step*i)
                 y_r -= repulsion * math.sin(angle_min + psi + step*i)
             else :
@@ -106,6 +107,12 @@ class Planner:
             self.v_repel = np.zeros(2)
         else :
             self.v_repel = np.array([x_r, y_r])
+
+        d = (self.x_goal - self.x)**2 + (self.y_goal - self.y)**2
+        if d < scan_data.range_min ** 2 :
+            self.v_repel = np.zeros(2)
+        if d < 0.01 :
+            self.v_repel = np.zeros(2)
 
     def planner(self):
         if self.scan_data is None :
